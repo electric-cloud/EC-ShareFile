f91eb6248365a11832035373209ca29888bd8862
lrochette <lrochette@electric-cloud.com>
2015-07-22 14:35:48 -0700

    Release with Configuration

--- src/project/manifest.pl
+++ src/project/manifest.pl
@@ -1,5 +1,8 @@
 @files = (
 	['//project/propertySheet/property[propertyName="ec_setup"]/value', 'ec_setup.pl'],
-	['//project/procedure[procedureName="CreateFolderAndUploadFile"]/propertySheet/property[propertyName="ec_parameterForm"]/value', 'procedures/CreateFolderAndUploadFile/form.xml'],
-	['//project/procedure[procedureName="CreateFolderAndUploadFile"]/step[stepName="createFolderAndUploadFile"]/command', 'procedures/CreateFolderAndUploadFile/steps/createFolderAndUploadFile.sh'],
+	['//project/procedure[procedureName="createConfiguration"]/step[stepName="CreateConfiguration"]/command', 'procedures/createConfiguration/steps/CreateConfiguration.pl'],
+	['//project/procedure[procedureName="createConfiguration"]/step[stepName="createAndAttachCredential"]/command', 'procedures/createConfiguration/steps/createAndAttachCredential.pl'],
+	['//project/procedure[procedureName="createFolderAndUploadFile"]/propertySheet/property[propertyName="ec_parameterForm"]/value', 'procedures/createFolderAndUploadFile/form.xml'],
+	['//project/procedure[procedureName="createFolderAndUploadFile"]/step[stepName="createFolderAndUploadFile"]/command', 'procedures/createFolderAndUploadFile/steps/createFolderAndUploadFile.sh'],
+	['//project/procedure[procedureName="DeleteConfiguration"]/step[stepName="DeleteConfiguration"]/command', 'procedures/DeleteConfiguration/steps/DeleteConfiguration.pl'],
 );
