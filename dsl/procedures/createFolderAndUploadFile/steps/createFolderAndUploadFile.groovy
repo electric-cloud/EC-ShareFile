73d1f1eba3e0397d7e6d7410e61e3975ea573647
Laurent Rochette <lrochette@eletric-cloud.com>
2018-08-31 21:56:48 -0500

    Convert to PluginWizard Convert to ec-groovy API

---
+++ dsl/procedures/createFolderAndUploadFile/steps/createFolderAndUploadFile.groovy
@@ -0,0 +1,32 @@
+import com.electriccloud.client.groovy.ElectricFlow
+$[/myProject/preamble]
+
+  public static void main(String[] args) throws Exception {
+
+    ElectricFlow ef = new ElectricFlow()
+
+    // Get Credentials
+    def resp = ef.getFullCredential(credentialName: '$[config]')
+    def userName = resp.credential.userName
+    def password = resp.credential.password
+
+    def company=ef.getProperty(propertyName:"/myProject/ec_plugin_cfgs/$[config]/company").value
+
+    def folderToCreate = ef.getProperty(propertyName:"/myProject/ec_plugin_cfgs/$[config]/folder").value
+    def list = folderToCreate.split("/")
+    def lastItem = list.last()
+    def path = folderToCreate - ~/$lastItem/
+
+    ShareFileSample sample = new ShareFileSample();
+    HashMap<String, Object> optionalParameters = new HashMap<String, Object>();
+
+    boolean loginStatus = sample.authenticate("$company", "sharefile.com", "$userName", "$password");
+    if (loginStatus)
+    {
+        String id = sample.folderCreate(lastItem, path);
+        optionalParameters.put("folderid",id);
+        sample.fileUpload("$[pathToFile]", optionalParameters)
+    }
+  }
+
+}
---
+++ ecpluginbuilder.pl
@@ -0,0 +1,63 @@
+#!/usr/bin/env perl
+
+# Build, upload and promote EC-Support using ecpluginbuilder
+#		https://github.com/electric-cloud/ecpluginbuilder
+
+use Getopt::Long;
+use Data::Dumper;
+use strict;
+use File::Copy;
+
+use ElectricCommander ();
+$| = 1;
+my $ec = new ElectricCommander->new();
+
+my $epb="../ecpluginbuilder";
+
+my $pluginVersion = "1.3.0";
+my $pluginKey = "EC-ShareFile";
+
+GetOptions ("version=s" => \$pluginVersion)
+		or die (qq(
+Error in command line arguments
+
+	createPlugin.pl
+		[--version <version>]
+		)
+);
+
+# Read buildCounter
+my $buildCounter;
+{
+  local $/ = undef;
+  open FILE, "buildCounter" or die "Couldn't open file: $!";
+  $buildCounter = <FILE>;
+  close FILE;
+
+ $buildCounter++;
+ $pluginVersion .= ".$buildCounter";
+ print "[INFO] - Incrementing build number to $buildCounter...\n";
+
+ open FILE, "> buildCounter" or die "Couldn't open file: $!";
+ print FILE $buildCounter;
+ close FILE;
+}
+my $pluginName = "${pluginKey}-${pluginVersion}";
+
+print "[INFO] - Creating plugin '$pluginName'\n";
+
+system ("$epb -pack-jar -plugin-name $pluginKey -plugin-version $pluginVersion " .
+ " -folder META-INF" .
+ " -folder dsl" .
+ " -folder htdocs" .
+ " -folder pages");
+
+move("build/${pluginKey}.jar", ".");
+
+# Install plugin
+print "[INFO] - Installing plugin ${pluginKey}.jar...\n";
+$ec->installPlugin("${pluginKey}.jar");
+
+# Promote plugin
+print "[INFO] - Promoting plugin...\n";
+$ec->promotePlugin($pluginName);
